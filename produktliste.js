620e746ae7a5747eac1cf223faae27ee393e87a5
Mathilde <you@example.com>
2024-02-07 20:21:29 +0100

    soldout

--- produktliste.js
+++ produktliste.js
@@ -8,7 +8,7 @@ function showProducts(products) {
 }
 
 function showProduct(product) {
-  //console.log("test");
+  console.log("test");
   //clone, ændre og appende
   const template = document.querySelector("#smallProductTemplate").content;
   const copy = template.cloneNode(true);
@@ -17,11 +17,19 @@ function showProduct(product) {
   copy.querySelector("h3").textContent = product.productdisplayname;
   copy.querySelector("h4").textContent = product.brandname;
   copy.querySelector("h5").textContent = "Prev. DKK " + product.price + ",-";
-  copy.querySelector("div.discounted").textContent = "-" + product.discount + "%";
 
+  //hvis produktet er udsolgt
   if (product.soldout) {
     copy.querySelector("article").classList.add("soldOut");
+    copy.querySelector("div.soldout").textContent = "Udsolgt";
   }
+
+  //hvis produktet er på udsalg
+  if (product.discounted) {
+    copy.querySelector("article").classList.add("discount");
+    copy.querySelector("div.discounted").textContent = "-" + product.discount + "%";
+  }
+
   copy.querySelector(".link_produkt").setAttribute("href", `produkt.html?id=${product.id}`);
 
   //appende
